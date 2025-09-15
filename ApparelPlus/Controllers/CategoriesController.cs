8babf7467253874bb749c0c3a22abefe21045b4e
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-09-15 12:51:27 -0400

    Add project files.

---
+++ ApparelPlus/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using ApparelPlus.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApparelPlus.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // create mock in-memory list of Categories (replaced next week with db)
+            var categories = new List<Category>();
+
+            for (int i = 1; i < 11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // load the view and pass the category list for display
+            return View(categories);
+        }
+    }
+}
---
+++ ApparelPlus/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ApparelPlus.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
