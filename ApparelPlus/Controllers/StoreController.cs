1cfbce168d46181960c18c9bcdaa40197df22921
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-11-03 12:51:59 -0500

    l9/1 - starting cart

--- ApparelPlus/Controllers/StoreController.cs
+++ ApparelPlus/Controllers/StoreController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApparelPlus.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApparelPlus.Controllers
 {
     public class StoreController : Controller
     {
+        // shared db connection
+        private readonly ApplicationDbContext _context;
+
+        // constructor to instantiate db connection
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Store => show Categories so user can choose 1
         public IActionResult Index()
         {
-            return View();
+            // fetch Category list
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+
+            // show view and pass Category list data for display
+            return View(categories);
+        }
+
+        // GET: /Store/Products/5 => show Products in selected Category
+        public IActionResult Products(int id)
+        {
+            // fetch Category to display the Name as the page heading & title
+            var category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Category"] = category.Name;
+
+            // fetch products in this category
+            var products = _context.Products
+                .Where(p => p.CategoryId == id)
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            return View(products);
         }
     }
 }
