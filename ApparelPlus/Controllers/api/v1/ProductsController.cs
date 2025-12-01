ead851194de80763cff4242a1b20662c7b89e9fe
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-12-01 13:30:10 -0500

    l13/2 - POST method

--- ApparelPlus/Controllers/api/v1/ProductsController.cs
+++ ApparelPlus/Controllers/api/v1/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApparelPlus.Data;
+using ApparelPlus.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 
@@ -8,13 +10,55 @@ namespace ApparelPlus.Controllers.api.v1
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        // shared db conn
+        private readonly ApplicationDbContext _context;
 
-        // GET: /api/v1/products
+        // constructor w/db dependency
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /api/v1/products => fetch all products
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok("Products will go here next");
+            // fetch products from db
+            var products = _context.Products.ToList();
+
+            // return product data as json
+            return Ok(products);
         }
 
+        // GET: /api/v1/products/22 => fetch selected product
+        [HttpGet("{id}")]
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // POST: /api/v1/products => create new product
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            // validate
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(); // 400 status
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            // return 201 resource created
+            return CreatedAtAction("Create", product);
+        }
     }
 }
